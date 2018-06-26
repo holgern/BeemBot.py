2125d319863c51645d168369805d8b3a9ab1ae21
Holger <holger@nahrstaedt.de>
2018-06-26 16:08:08 +0200

    Fix beem

--- Cogs/Beem.py
+++ Cogs/Beem.py
@@ -28,9 +28,9 @@ class Beem:
     @commands.command(pass_context=True)
     async def account(self, ctx, *, account : str = None):
         """Retuns information about an account"""
-        if member is None:
-            member = ctx.message.author
-            member = str(member).split('#')[0]
-        a = Account(member, steem_instance=self.stm)
+        if account is None:
+            account = ctx.message.author
+            account = str(account).split('#')[0]
+        a = Account(account, steem_instance=self.stm)
         response = a.print_info(return_str=True)
         await ctx.channel.send("```" + response + "```")
